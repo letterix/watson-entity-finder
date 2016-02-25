bf0bfbd2bc7ecd2f66a2e99283c352735006c759
Rasmus Letterkrantz <rasmus.letterkrantz@gmail.com>
2016-02-25 13:01:25 +0100

    added example usage of the utility function

--- app/chain/alchemy/alchemy.resource.js
+++ app/chain/alchemy/alchemy.resource.js
@@ -9,6 +9,7 @@ var Promise = require('bluebird');
 var config = require('config');
 var responseHandler = require('../../handler/response.handler');
 var errorHandler = require('../../handler/error.handler');
+var utils  = require('../../utility/utils');
 
 var url = config.RESOURCE_ALCHEMY_URL;
 var apikey = config.RESOURCE_ALCHEMY_API_KEY;
@@ -16,15 +17,22 @@ var apikey = config.RESOURCE_ALCHEMY_API_KEY;
 exports.get = function() {
     var options = {
         resolveWithFullResponse: true,
-        uri: url + '?' + 'apiKey=' + apikey + '&outputMode=json' + '&url=' + url,
+        uri : url,
+        //uri: url + '?' + 'apiKey=' + apikey + '&outputMode=json' + '&url=' + url,
         method: 'GET',
         json: true,
         gzip: true
     };
-    
-    
-    return request(options)
+
+    var params = {
+        "url": "https://en.wikipedia.org/wiki/Steve_Jobs",
+        "apikey": apikey,
+        "outputMode": "json"
+    };
+
+    return utils.setUrlParamsForOptions(params, options)
+        .then(request)
         .then(responseHandler.parseGet)
         .catch(errorHandler.throwResourceError);
-    
+
 };
