0fdf4f0a8de7f2bac8a242b9a8cb39088d768b8b
pontusthome <pontusthome@gmail.com>
2016-02-25 11:15:47 +0100

    Fixed variable names on scopus.resources.js

--- app/chain/scopus/scopus.resource.js
+++ app/chain/scopus/scopus.resource.js
@@ -11,12 +11,12 @@ var responseHandler = require('../../handler/response.handler');
 var errorHandler = require('../../handler/error.handler');
 
 var scopusSearchUrl = config.RESOURCE_SCOPUS_SEARCH_URL;
-var apikey = config.RESOURCE_SCOPUS_API_KEY;
+var apiKey = config.RESOURCE_SCOPUS_API_KEY;
 
 exports.get = function() {
     var options = {
         resolveWithFullResponse: true,
-        uri: url + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json' + '&query=wilsons',
+        uri: scopusSearchUrl + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json' + '&query=wilsons',
         method: 'GET',
         json: true,
         gzip: true
