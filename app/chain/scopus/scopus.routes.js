d7ea682d70a970d4c2d24cbb447da7e6e831d1a6
pontusthome <pontusthome@gmail.com>
2016-02-25 11:08:24 +0100

    Added routes for scopus chain

---
+++ app/chain/scopus/scopus.routes.js
@@ -0,0 +1,20 @@
+/*eslint-env node */
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var responseHandler = require('../../handler/response.handler');
+var scopusController = require('./scopus.controller');
+
+module.exports = function(routes) {
+
+    // fetch something from the controller
+    routes.get('/', function(request, response) {
+        scopusController.get()
+            .then(responseHandler.sendJsonResponse(response))
+            .catch(responseHandler.sendErrorResponse(response));
+    });
+
+    return routes;
+};
