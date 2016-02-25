fb5b1f78fa2e836340b788584775c0a361e5c881
pontusthome <pontusthome@gmail.com>
2016-02-25 11:55:07 +0100

    Added searchAuthor function to scopus.resource

--- app/chain/scopus/scopus.resource.js
+++ app/chain/scopus/scopus.resource.js
@@ -11,6 +11,7 @@ var responseHandler = require('../../handler/response.handler');
 var errorHandler = require('../../handler/error.handler');
 
 var scopusSearchUrl = config.RESOURCE_SCOPUS_SEARCH_URL;
+var scopusSearchAuthorUrl = config.RESOURCE_SCOPUS_SEARCH_AUTHOR_URL;
 var apiKey = config.RESOURCE_SCOPUS_API_KEY;
 
 exports.search = function(search) {
@@ -25,5 +26,20 @@ exports.search = function(search) {
     return request(options)
         .then(responseHandler.parseGet)
         .catch(errorHandler.throwResourceError);
+};
+
+exports.authorSearch = function(search) {
+
+    var options = {
+        resolveWithFullResponse: true,
+        uri: scopusSearchAuthorUrl + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json' + '&query=' + search,
+        method: 'GET',
+        json: true,
+        gzip: true
+    };
     
+    return request(options)
+        .then(responseHandler.parseGet)
+        .catch(errorHandler.throwResourceError);    
+
 };
