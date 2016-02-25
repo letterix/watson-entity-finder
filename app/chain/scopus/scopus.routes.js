c0afb267b084a3a335c195aa432d8c27e8f3429a
pontusthome <pontusthome@gmail.com>
2016-02-25 12:28:48 +0100

    Added authorSearch to scopus.routes at /author/

--- app/chain/scopus/scopus.routes.js
+++ app/chain/scopus/scopus.routes.js
@@ -8,10 +8,18 @@ var responseHandler = require('../../handler/response.handler');
 var scopusController = require('./scopus.controller');
 
 module.exports = function(routes) {
-
-    // fetch something from the controller
     routes.get('/:search', function(request, response) {
         scopusController.search(request.params.search)
+            .then(responseHandler.sendJsonResponse(response))
+            .catch(responseHandler.sendErrorResponse(response));
+    });
+
+    return routes;
+};
+
+module.exports = function(routes) {
+    routes.get('/author/', function(request, response) {
+        scopusController.authorSearch()
             .then(responseHandler.sendJsonResponse(response))
             .catch(responseHandler.sendErrorResponse(response));
     });
