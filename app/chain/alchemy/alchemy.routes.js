847df6bfcbfdfcc560d718fd7d6c66ac83ef7ec3
pontusthome <pontusthome@gmail.com>
2016-02-26 10:28:00 +0100

    Added a route to getEntities to alchemy routes

--- app/chain/alchemy/alchemy.routes.js
+++ app/chain/alchemy/alchemy.routes.js
@@ -16,5 +16,19 @@ module.exports = function(routes) {
             .catch(responseHandler.sendErrorResponse(response));
     });
 
+    // fetch something from the controller
+    routes.get('/params', function(request, response) {
+    
+	    var params = {
+	        "url": "https://en.wikipedia.org/wiki/Steve_Jobs", 
+	        "apikey": "9da318e13054c45454b95f3d9db450041f69a507",
+	        "outputMode": "json"
+	    };
+
+        alchemyController.getEntities(params)
+            .then(responseHandler.sendJsonResponse(response))
+            .catch(responseHandler.sendErrorResponse(response));
+    });
+
     return routes;
 };
