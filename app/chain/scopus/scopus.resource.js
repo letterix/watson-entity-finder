d35ae79b07ee928c73d8742e02ecc68fb3144874
pontusthome <pontusthome@gmail.com>
2016-02-29 08:14:43 +0100

    Changed all function so that the function parameter can be used to make the search. Changed all functions to use params.

--- app/chain/scopus/scopus.resource.js
+++ app/chain/scopus/scopus.resource.js
@@ -9,6 +9,7 @@ var Promise = require('bluebird');
 var config = require('config');
 var responseHandler = require('../../handler/response.handler');
 var errorHandler = require('../../handler/error.handler');
+var utils  = require('../../utility/utils');
 
 var scopusSearchUrl = config.RESOURCE_SCOPUS_SEARCH_URL;
 var scopusSearchAuthorUrl = config.RESOURCE_SCOPUS_SEARCH_AUTHOR_URL;
@@ -17,51 +18,79 @@ var scopusRetrieveAbstractUrl = config.RESOURCE_SCOPUS_RETRIEVE_ABSTRACT_URL;
 var apiKey = config.RESOURCE_SCOPUS_API_KEY;
 
 exports.search = function(search) {
+    var params = {
+        "apiKey": apiKey,
+        "httpAccept": "application/json",
+        "query": search
+    };
+    
     var options = {
         resolveWithFullResponse: true,
-        uri: scopusSearchUrl + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json' + '&query=' + search,
+        uri: scopusSearchUrl,
         method: 'GET',
         json: true,
         gzip: true
     };
     
-    return request(options)
+    return utils.setUrlParamsForOptions(params, options)
+        .then(request)
         .then(responseHandler.parseGet)
         .catch(errorHandler.throwResourceError);
 };
 
 exports.authorSearch = function(search) {
+    var params = {
+        "apiKey": apiKey,
+        "httpAccept": "application/json",
+        "query": search
+    };
+
     var options = {
         resolveWithFullResponse: true,
-        uri: scopusSearchAuthorUrl + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json' + '&query=AUTHLASTNAME(King)%20and%20AUTHFIRST(W)%20and%20AFFIL(University)%20and%20AFFIL(Queensland)%20and%20AFFIL(Bishop%27s)',
+    //    uri: scopusSearchAuthorUrl + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json' + '&query=AUTHLASTNAME(King)%20and%20AUTHFIRST(W)%20and%20AFFIL(University)%20and%20AFFIL(Queensland)%20and%20AFFIL(Bishop%27s)',
+        uri: scopusSearchAuthorUrl,
         method: 'GET',
         json: true,
         gzip: true
     };
-    
-    return request(options)
+
+    return utils.setUrlParamsForOptions(params, options)
+        .then(request)
         .then(responseHandler.parseGet)
-        .catch(errorHandler.throwResourceError);    
+        .catch(errorHandler.throwResourceError); 
 };
 
 exports.retrieveAuthor = function(search) {
+    var params = {
+        "apiKey": apiKey,
+        "httpAccept": "application/json",
+    };
+
     var options = {
         resolveWithFullResponse: true,
-        uri: scopusRetrieveAuthorUrl + '56218921200' + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json',
+    //    uri: scopusRetrieveAuthorUrl + '56218921200' + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json',
+        uri: scopusRetrieveAuthorUrl + search,
         method: 'GET',
         json: true,
         gzip: true
     };
     
-    return request(options)
+    return utils.setUrlParamsForOptions(params, options)
+        .then(request)
         .then(responseHandler.parseGet)
-        .catch(errorHandler.throwResourceError);    
+        .catch(errorHandler.throwResourceError);  
 };
 
 exports.retrieveAbstract = function(search) {
+    var params = {
+        "apiKey": apiKey,
+        "httpAccept": "application/json",
+    };
+
     var options = {
         resolveWithFullResponse: true,
-        uri: scopusRetrieveAbstractUrl + '10.1016/j.anbehav.2015.12.020' + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json',
+    //    uri: scopusRetrieveAbstractUrl + '10.1016/j.anbehav.2015.12.020' + '?' + 'apiKey=' + apiKey + '&httpAccept=application/json',
+        uri: scopusRetrieveAbstractUrl + search,
         method: 'GET',
         json: true,
         gzip: true
