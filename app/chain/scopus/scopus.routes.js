cad0d1c8f28454f452f9f0c2f57d241c8c6ceda8
pontusthome <pontusthome@gmail.com>
2016-02-29 08:13:29 +0100

    Added hard coded tests for the scopus routes

--- app/chain/scopus/scopus.routes.js
+++ app/chain/scopus/scopus.routes.js
@@ -8,26 +8,26 @@ var responseHandler = require('../../handler/response.handler');
 var scopusController = require('./scopus.controller');
 
 module.exports = function(routes) {
-    routes.get('/:search', function(request, response) {
+    routes.get('/search/:search', function(request, response) {
         scopusController.search(request.params.search)
             .then(responseHandler.sendJsonResponse(response))
             .catch(responseHandler.sendErrorResponse(response));
-    });
+    }); 
 
     routes.get('/author/', function(request, response) {
-        scopusController.authorSearch()
+        scopusController.authorSearch('AUTHLASTNAME(King)%20and%20AUTHFIRST(W)%20and%20AFFIL(University)%20and%20AFFIL(Queensland)%20and%20AFFIL(Bishop%27s)')
             .then(responseHandler.sendJsonResponse(response))
             .catch(responseHandler.sendErrorResponse(response));
     });
 
     routes.get('/retrieveAuthor/', function(request, response) {
-        scopusController.retrieveAuthor()
+        scopusController.retrieveAuthor('56218921200')
             .then(responseHandler.sendJsonResponse(response))
             .catch(responseHandler.sendErrorResponse(response));
     });
 
     routes.get('/retrieveAbstract/', function(request, response) {
-        scopusController.retrieveAbstract()
+        scopusController.retrieveAbstract('10.1016/j.anbehav.2015.12.020')
             .then(responseHandler.sendJsonResponse(response))
             .catch(responseHandler.sendErrorResponse(response));
     });
