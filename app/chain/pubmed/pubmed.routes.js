a4d07e6e78effa4c5535394134786b4b72f58b6b
Fredrik Rahn <rahn.fredrik@gmail.com>
2016-03-01 15:50:05 +0100

    Added routes for functions in resource

---
+++ app/chain/pubmed/pubmed.routes.js
@@ -0,0 +1,24 @@
+/*eslint-env node */
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var responseHandler = require('../../handler/response.handler');
+var pubmedController = require('./pubmed.controller');
+
+module.exports = function(routes) {
+    routes.get('/:searchPMID', function(request, response) {
+        pubmedController.searchPMID(request.params.search)
+            .then(responseHandler.sendJsonResponse(response))
+            .catch(responseHandler.sendErrorResponse(response));
+    });
+
+    routes.get('/retrieveAbstract/:pmid', function(request, response) {
+        pubmedController.retrieveAbstract(request.params.pmid)
+            .then(responseHandler.sendTextResponse(response))
+            .catch(responseHandler.sendErrorResponse(response));
+    });
+
+    return routes;
+};
