6f6c46280a32cdd4630f63a5854d7f2dc9d8f939
pontusthome <pontusthome@gmail.com>
2016-03-01 14:26:38 +0100

    Added retrieveFullText to resource

--- app/chain/scidir/scidir.resource.js
+++ app/chain/scidir/scidir.resource.js
@@ -12,12 +12,28 @@ var errorHandler = require('../../handler/error.handler');
 var utils = require('../../utility/utils');
 
 var scidirSearchUrl = config.RESOURCE_SCIDIR_SEARCH_URL;
+var scidirRetrieveFullText = config.RESOURCE_SCIDIR_FULL_TEXT_URL;
 var apiKey = config.RESOURCE_SCOPUS_API_KEY;
 
 exports.search = function(params) {
     var options = {
         resolveWithFullResponse: true,
         uri: scidirSearchUrl,
+        method: 'GET',
+        json: true,
+        gzip: true
+    };
+
+    return utils.setUrlParamsForOptions(params, options)
+        .then(request)
+        .then(responseHandler.parseGet)
+        .catch(errorHandler.throwResourceError);
+};
+
+exports.retrieveFullText = function(pii, params) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: scidirRetrieveFullText + pii,
         method: 'GET',
         json: true,
         gzip: true
