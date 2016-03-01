b9df70ccc7d2d92f0abfc6fde58b74396291e2b2
pontusthome <pontusthome@gmail.com>
2016-03-01 13:45:15 +0100

    added science direct search in scidir routes

---
+++ app/chain/scidir/scidir.routes.js
@@ -0,0 +1,18 @@
+/*eslint-env node */
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var responseHandler = require('../../handler/response.handler');
+var scidirController = require('./scidir.controller');
+
+module.exports = function(routes) {
+    routes.get('/:search', function(request, response) {
+        scidirController.search(request.params.search)
+            .then(responseHandler.sendJsonResponse(response))
+            .catch(responseHandler.sendErrorResponse(response));
+    });
+
+    return routes;
+};
