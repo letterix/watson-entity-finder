153d2c60cabca53fecec926a481b4820d94b8957
Pontus Thomé <pontusthome@gmail.com>
2016-03-28 12:28:34 +0200

    Set up the colums to fit our input with citedBy, IPP, SNIP and SJR

--- app/chain/tradeoff-analytics/tradeoff.controller.js
+++ app/chain/tradeoff-analytics/tradeoff.controller.js
@@ -11,101 +11,63 @@ var errorHandler = require('../../handler/error.handler.js');
 // DOES EXPORT
 // ====================================================
 
-exports.getDilemmas = function() {
-    /*
-    columns = {
+exports.getDilemmas = function(options) {
+    var columns = [
+        {
+            "type": "numeric",
+            "key": "citedBy",
+            "full_name": "Cited by - count",
+            "range": {
+              "low": 0,
+              "high": 400
+            },
+            "format": "####0",
+            "goal": "max",
+            "is_objective": true
+        },
+        {
+            "type": "numeric",
+            "key": "IPP",
+            "full_name": " Impact per Publication",
+            "range": {
+              "low": 0.000,
+              "high": 10.000
+            },
+            "format": "0.000",
+            "goal": "max",
+            "is_objective": true
+        },
+        {
+            "type": "numeric",
+            "key": "SJR",
+            "full_name": "SCImago Journal Rank",
+            "range": {
+              "low": 0.000,
+              "high": 10.000
+            },
+            "format": "0.000",
+            "goal": "max",
+            "is_objective": true
+        },        
+        {
+            "type": "numeric",
+            "key": "SNIP",
+            "full_name": "Source Normalized Impact per Paper",
+            "range": {
+              "low": 0.000,
+              "high": 10.000
+            },
+            "format": "0.000",
+            "goal": "max",
+            "is_objective": true
+        }
+    ];
+    var subject = "Researcher Ranking";
 
-    };
-    options = {
-
-    };
-    subject = {
-
-    };
-*/
     var problem = {
-        columns: [
-            {
-                "type": "numeric",
-                "key": "price",
-                "full_name": "Price",
-                "range": {
-                    "low": 0.0,
-                    "high": 400.0
-                },
-                "format": "$####0.00",
-                "goal": "min",
-                "is_objective": true
-            },
-            {
-                "type": "numeric",
-                "key": "weight",
-                "full_name": "Weight",
-                "format": "####0 g",
-                "goal": "min",
-                "is_objective": true
-            },
-            {
-                "type": "categorical",
-                "key": "brand",
-                "full_name": "Brand",
-                "range": [
-                    "Apple",
-                    "HTC",
-                    "Samsung"
-                ],
-                "goal": "min",
-                "preference": [
-                    "Samsung",
-                    "Apple",
-                    "HTC"
-                ],
-                "is_objective": true
-            },
-            {
-                "type": "datetime",
-                "key": "rDate",
-                "full_name": "Release Date",
-                "goal": "max",
-                "is_objective": false
-            }
-        ],
-        "subject": "phones",
-        "options": [
-            {
-                "key": "1",
-                "name": "Samsung Galaxy S4",
-                "values": {
-                    "weight": 130,
-                    "brand": "Samsung",
-                    "price": 249,
-                    "rDate": "2013-04-29T00:00:00Z"
-                },
-                "description_html": ""
-            },
-            {
-                "key": "2",
-                "name": "Apple iPhone 5",
-                "values": {
-                    "weight": 112,
-                    "brand": "Apple",
-                    "price": 449,
-                    "rDate": "2012-09-21T00:00:00Z"
-                },
-                "description_html": ""
-            },
-            {
-                "key": "3",
-                "name": "HTC One",
-                "values": {
-                    "weight": 143,
-                    "brand": "HTC",
-                    "price": 299,
-                    "rDate": "2013-03-01T00:00:00Z"
-                },
-                "description_html": ""
-            }
-        ]
+        columns: columns,
+        subject: subject,
+        options: options
     };
 
     return tradeoffResource.getDilemmas(problem);
