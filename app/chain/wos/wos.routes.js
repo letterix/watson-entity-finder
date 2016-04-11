341f11b04705457f8f96eb7ac37239716cc7c564
Pontus Thomé <pontusthome@gmail.com>
2016-04-11 17:26:47 +0200

    Created routes for wos

---
+++ app/chain/wos/wos.routes.js
@@ -0,0 +1,20 @@
+/*eslint-env node */
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var responseHandler = require('../../handler/response.handler');
+var wosController = require('./wos.controller');
+
+module.exports = function(routes) {
+
+    // This just a hard coded test
+    routes.get('/', function(request, response) {
+        wosController.search()
+            .then(responseHandler.sendJsonResponse(response))
+            .catch(responseHandler.sendErrorResponse(response));
+    });
+
+    return routes;
+};
