adbf446f59af357353bf931145234f7c0ee13e29
FredrikRahn <rahnf@student.chalmers.se>
2016-04-11 16:07:37 +0200

    Changed name of request param to match controller

--- app/chain/pubmed/pubmed.routes.js
+++ app/chain/pubmed/pubmed.routes.js
@@ -8,8 +8,8 @@ var responseHandler = require('../../handler/response.handler');
 var pubmedController = require('./pubmed.controller');
 
 module.exports = function(routes) {
-    routes.get('/:searchPMID/:search', function(request, response) {
-        pubmedController.searchPMID(request.params.search)
+    routes.get('/searchPMID/:query', function(request, response) {
+        pubmedController.searchPMID(request.params.query)
             .then(responseHandler.sendJsonResponse(response))
             .catch(responseHandler.sendErrorResponse(response));
     });
