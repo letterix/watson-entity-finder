eca346f286d505a014e93b22a0411bd96e5fa217
Pontus Thomé <pontusthome@gmail.com>
2016-04-11 17:27:56 +0200

    Created a function for requesting authentication in wos resource

---
+++ app/chain/wos/wos.resource.js
@@ -0,0 +1,33 @@
+/*eslint-env node */
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var request = require('request-promise');
+var Promise = require('bluebird');
+var config = require('config');
+var responseHandler = require('../../handler/response.handler');
+var errorHandler = require('../../handler/error.handler');
+
+var url = 'http://search.webofknowledge.com/esti/wokmws/ws/WOKMWSAuthenticate?wsdl';
+
+exports.search = function(xml) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri : url,
+        method: 'POST',
+        headers: {
+            'content-type': 'text/xml;charset=UTF-8'
+        },
+        form: xml,
+        gzip: true
+    };
+
+    return request(options)
+        .then(responseHandler.parsePostXml)
+        .catch(errorHandler.throwResourceError);
+
+};
+
+
