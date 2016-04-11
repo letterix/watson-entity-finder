7ad9a6729afe8e59a49f1deefcaf38e7b293be1e
Fredrik Rahn <rahn.fredrik@gmail.com>
2016-04-11 14:51:25 +0200

    Added search term

--- app/chain/pubmed/pubmed.routes.js
+++ app/chain/pubmed/pubmed.routes.js
@@ -8,7 +8,7 @@ var responseHandler = require('../../handler/response.handler');
 var pubmedController = require('./pubmed.controller');
 
 module.exports = function(routes) {
-    routes.get('/:searchPMID', function(request, response) {
+    routes.get('/:searchPMID/:search', function(request, response) {
         pubmedController.searchPMID(request.params.search)
             .then(responseHandler.sendJsonResponse(response))
             .catch(responseHandler.sendErrorResponse(response));
