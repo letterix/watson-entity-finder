0efae0ec7591b16d040de93b19b041c1a9d74be6
Pontus Thomé <pontusthome@gmail.com>
2016-04-19 16:19:34 +0200

    Added retrieveAuthorBatch for scopus resource

--- app/chain/scopus/scopus.resource.js
+++ app/chain/scopus/scopus.resource.js
@@ -13,10 +13,11 @@ var utils = require('../../utility/utils');
 
 var scopusSearchUrl = config.RESOURCE_SCOPUS_SEARCH_URL;
 var scopusSearchAuthorUrl = config.RESOURCE_SCOPUS_SEARCH_AUTHOR_URL;
-var scopusRetrieveAuthorUrl = config.RESOURCE_SCOPUS_RETRIEVE_AUTHOR_URL;
+var scopusRetrieveAuthorUrl = config.RESOURCE_SCOPUS_RETRIEVE_AUTHOR_BATCH_URL + '/author_id/';
+var scopusRetrieveAuthorBatchUrl = config.RESOURCE_SCOPUS_RETRIEVE_AUTHOR_BATCH_URL;
 var scopusRetrieveAbstractUrl = config.RESOURCE_SCOPUS_RETRIEVE_ABSTRACT_URL;
 var scopusRetrieveArticleUrl = config.RESOURCE_SCOPUS_RETRIEVE_ARTICLE_URL;
-var scopusRetrieveIssnUrl = config.RESOURCE_SCOPUS_RETRIEVE_ISSN_URL;
+var scopusRetrieveIssnUrl = config.RESOURCE_SCOPUS_RETRIEVE_ISSN_BATCH_URL + '/issn/';
 var scopusRetrieveIssnBatchUrl = config.RESOURCE_SCOPUS_RETRIEVE_ISSN_BATCH_URL;
 var apiKey = config.RESOURCE_SCOPUS_API_KEY;
 
@@ -70,6 +71,26 @@ exports.retrieveAuthor = function(id, params) {
         .then(request)
         .then(responseHandler.parseGet)
         .catch(errorHandler.throwResourceError);
+};
+
+exports.retrieveAuthorBatch = function(params) {
+    var options = {
+        resolveWithFullResponse: true,
+        uri: scopusRetrieveAuthorBatchUrl,
+        method: 'GET',
+        agent: false,
+        json: true,
+        gzip: true
+    };
+    console.time('retrieveAuthorBatch');
+
+    return utils.setUrlParamsForOptions(params, options)
+        .then(request)
+        .then(function(res) {
+            console.timeEnd('retrieveAuthorBatch');
+            return res;
+        })
+        .then(responseHandler.parseGet);
 };
 
 exports.retrieveAbstract = function(title, params) {
