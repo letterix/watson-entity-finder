e6994a5f2073788642f6565f451a3d8c2e333bf9
Hannes <h.haggander@gmail.com>
2016-05-05 15:03:04 +0200

    added url link and cred, does connect to bluemix now!

--- app/chain/watson/conceptInsight/conecptInsight.resource.js
+++ app/chain/watson/conceptInsight/conecptInsight.resource.js
@@ -10,17 +10,20 @@ var responseHandler = require('../../../handler/response.handler');
 
 var watson = require('watson-developer-cloud');
 var conceptInsight = watson.concept_insights({
+	url: config.RESOURCE_CONCEPTINSIGHT_URL,
 	username: config.RESOURCE_CONCEPTINSIGHT_USERNAME,
 	password: config.RESOURCE_CONCEPTINSIGHT_PASSWORD,
 	version: 'v2'
 });
 
 exports.testAccount = function(){
+	console.log('testing connection and account cred with bluemix...');
 	conceptInsight.accounts.getAccountsInfo({}, function(err,res){
 		if(err){
 			console.log(err);
 
 		} else {
+			console.log('success!');
 			var accInfo = JSON.stringify(res,null,2); 
 			console.log(accInfo);
 			
